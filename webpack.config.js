57ab1b0a99ea014d1326c5b02fc0df98aaa96774
Denis Gorbunov <volo_non_valeo@mail.ru>
2024-12-27 19:15:36 +0300

    Устанавливает и настраивает babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,18 @@ module.exports = {
       patterns: [{ from: 'public' }],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ]
+  }
 };

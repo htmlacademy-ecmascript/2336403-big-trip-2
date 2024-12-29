614b8482e8dd5e876a197237a2a4c29d314e9e12
Denis Gorbunov <volo_non_valeo@mail.ru>
2024-12-29 17:35:20 +0300

    Добавляет элемент сортировки и удалет элемент новое событие

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import NewEventButtonView from './view/new-event-button-view.js';
+//import NewEventButtonView from './view/new-event-button-view.js';
+import FilterView from './view/filters-view.js';
+import SortView from './view/sort-view.js';
 import { render } from './render.js';
 
-const siteTripMainElement = document.querySelector('.trip-main');
+//const pageHeader = document.querySelector('.page-header');
+//const siteTripMainElement = pageHeader.querySelector('.trip-main');
+const siteFiltersElement = document.querySelector('.trip-controls__filters');
+const siteTripEventsElement = document.querySelector('.trip-events');
 
-render(new NewEventButtonView(), siteTripMainElement);
+//render(new NewEventButtonView(), siteTripMainElement);
+render(new FilterView(), siteFiltersElement);
+render(new SortView(), siteTripEventsElement);
